b7bf27eff18689d717f5d6b4e23a93b4144247f7
Ершов Алексей Александрович <alexseier12345@gmail.com>
2022-10-18 11:34:28 +0300

    added redux-toolkit

---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {reducers} from './reducers';
+
+export const rootReducer = combineReducers(reducers);
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
+
+export type RootStore = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
